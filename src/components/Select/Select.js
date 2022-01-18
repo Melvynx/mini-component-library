ecba902123cab3e3641d368e4f00eafcca163f7e
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-01-18 21:40:40 +0100

    select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,77 +1,66 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
-// import { getDisplayedValue } from './Select.helpers';
+import { getDisplayedValue } from './Select.helpers';
 
-const MySelect = styled.select({
-  width: 'max-content',
-  background: COLORS.transparentGray15,
-  color: 'inherit',
-  border: 'none',
-  padding: '12px 24px 12px 16px',
-  fontSize: '1rem',
-  borderRadius: 8,
-  appearance: 'none',
-  display: 'inline-block',
+const NativeSelect = styled.select({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  opacity: 0,
+  cursor: 'pointer',
 });
+
+const CustomSelect = styled.div`
+  padding: 12px 40px 12px 16px;
+  fontsize: ${16 / 16} + 'rem';
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 const Wrapper = styled.div({
-  display: 'flex',
-  alignItems: 'center',
-  color: COLORS.gray700,
-  '&:hover': {
-    color: COLORS.black,
-  },
+  position: 'relative',
+  width: 'max-content',
+  isolation: 'isolate',
 });
 
-const MyIcon = styled(Icon)({
-  display: 'inline',
-  marginLeft: '-32px',
-  color: 'inherit',
+const IconWrapper = styled.div({
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  right: 10,
+  margin: 'auto',
+  height: 24,
+  width: 24,
   pointerEvents: 'none',
 });
 
-// from https://stackoverflow.com/questions/294040/how-to-expand-select-option-width-after-the-user-wants-to-select-an-option
-const updateSelectWidth = (event) => {
-  console.log(event);
-  const select = document.createElement('select');
-  const selectOption = document.createElement('option');
-
-  selectOption.textContent = event.target.options[event.target.selectedIndex].text;
-  select.style = 'visibility: hidden; position: fixed;';
-
-  select.appendChild(selectOption);
-  event.target.after(select);
-
-  const newSelectWidth = select.getBoundingClientRect().width;
-  event.target.style.width = `${newSelectWidth + 48}px`;
-  select.remove();
-};
-
 const Select = ({ label, value, onChange, children }) => {
-  const selectRef = useRef();
-
-  useEffect(() => {
-    if (!selectRef.current) return;
-    updateSelectWidth({ target: selectRef.current });
-  }, []);
-
+  const displayValue = getDisplayedValue(value, children);
   return (
     <Wrapper>
-      <MySelect
-        ref={selectRef}
-        aria-label={label}
-        value={value}
-        onChange={(event) => {
-          onChange(event);
-          updateSelectWidth(event);
-        }}
-      >
+      <NativeSelect value={value} onChange={onChange}>
         {children}
-      </MySelect>
-      <MyIcon id="chevron-down" />
+      </NativeSelect>
+      <CustomSelect>
+        {displayValue}
+        <IconWrapper>
+          <Icon id="chevron-down" size={24} strokeWidth={1} />
+        </IconWrapper>
+      </CustomSelect>
     </Wrapper>
   );
 };
